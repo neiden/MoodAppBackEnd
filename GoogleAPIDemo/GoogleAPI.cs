627c477f7f87067833a836bb80219cb77a76d6e4
neiden <pinkgodzela@gmail.com>
2023-03-21 15:17:47 -0700

    Added google sentiment analysis to API

---
+++ GoogleAPIDemo/GoogleAPI.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using Google.Cloud.Language.V1;
+
+namespace NaturalLanguageApi;
+
+public class GoogleAPI
+{
+    public static double AnalyzeSentiment(string text)
+    {
+        var client = LanguageServiceClient.Create();
+        var response = client.AnalyzeSentiment(Document.FromPlainText(text));
+        var sentiment = response.DocumentSentiment;
+        //Console.WriteLine($"Score: {sentiment.Score}");
+        //Console.WriteLine($"Magnitude: {sentiment.Magnitude}");
+        return sentiment.Score;
+    }
+}
