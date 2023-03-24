c7b526ccaae8aa05abce38d0caac9af66a54d59b
Divyarajsinh Rana <divyarajrana04@gmail.com>
2023-03-24 10:29:13 -0500

    added more tests for services

--- MoodAppApi/Tests/ServicesTests.cs
+++ MoodAppApi/Tests/ServicesTests.cs
@@ -31,5 +31,49 @@ namespace Tests
             Assert.Equal(expectedUser.Username, result.Username);
             Assert.Equal(expectedUser.Password, result.Password);
         }
+
+        public void GetAllUsers_Should_Return_ListOfUsers()
+        {
+            // Arrange
+            var expectedUsers = new List<Users>
+            {
+                new Users { User_Id = 1, F_Name = "John" },
+                new Users { User_Id = 2, F_Name = "Jane" }
+            };
+            _repoMock.Setup(repo => repo.GetAllUsers()).Returns(expectedUsers);
+
+            // Act
+            var result = _repoMock.Object.GetAllUsers();
+
+            // Assert
+            Assert.Equal(expectedUsers, result);
+        }
+
+        public void GetUserByUsername_Should_Return_Login()
+        {
+            // Arrange
+            var expectedLogin = new Login { Username = "john.doe", Pwd = "password" };
+            _repoMock.Setup(repo => repo.GetUserByUsername(expectedLogin.Username)).Returns(expectedLogin);
+
+            // Act
+            var result = _repoMock.Object.GetUserByUsername(expectedLogin.Username);
+
+            // Assert
+            Assert.Equal(expectedLogin, result);
+        }
+    
+        public void GetUserByUserID_Should_Return_User()
+        {
+            // Arrange
+            var expectedUser = new Users { User_Id = 1, Username = "John" };
+            _repoMock.Setup(repo => repo.GetUserByUserID(expectedUser.User_Id)).Returns(expectedUser);
+
+            // Act
+            var result = _repoMock.Object.GetUserByUserID(expectedUser.User_Id);
+
+            // Assert
+            Assert.Equal(expectedUser, result);
+        }
+
     }
 }
